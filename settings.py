ca5e90fcadbf13e2d723cedc665f957112abf086
patrykvan <patrykvan@gmail.com>
2018-12-26 00:16:58 +0100

    Add settings

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""
+Settings constants
+"""
+
+MAX_FPS = 60
+HEIGHT_RES = 800
+WIDTH_RES = 600
+IMAGES_DIR = "../images/"
+PADDLE_IMG = IMAGES_DIR + "paddle.png"
+BALL_IMG = IMAGES_DIR + "ball.png"
