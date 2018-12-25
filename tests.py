108827817a7717f10dc2b71953f701d1685f2f2f
patrykvan <patrykvan@gmail.com>
2018-12-26 00:52:07 +0100

    Revert changes

--- tests.py
+++ tests.py
@@ -1,13 +1,9 @@
 import unittest
 
-
-# from load_utils import load_png
+from load_utils import load_png
 
 
 class TestLoadMethods(unittest.TestCase):
 
-    # def test_load_png_failure(self):
-    #     self.assertRaises(Exception, load_png, 'nonexistent_image.ping')
-
-    def test_travis(self):
-        self.assertTrue(True == False)
+    def test_load_png_failure(self):
+        self.assertRaises(SystemExit, load_png, 'nonexistent_image.ping')
