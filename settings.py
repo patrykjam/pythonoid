6c01055c8e0a315ca2b009399f95fa2afa502da3
patrykvan <patrykvan@gmail.com>
2018-12-26 22:59:57 +0100

    Update settings

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ Settings constants
 MAX_FPS = 60
 HEIGHT_RES = 800
 WIDTH_RES = 600
-IMAGES_DIR = "../images/"
+IMAGES_DIR = "images/"
 PADDLE_IMG = IMAGES_DIR + "paddle.png"
 BALL_IMG = IMAGES_DIR + "ball.png"
