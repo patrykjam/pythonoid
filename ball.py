0ba8c4f4749dca4e9e5d682cf2a460b82b340968
patrykvan <patrykvan@gmail.com>
2019-01-09 22:34:10 +0100

    Add super call

--- ball.py
+++ ball.py
@@ -15,7 +15,7 @@ class Ball(pygame.sprite.Sprite):
     """
 
     def __init__(self, area, vector):
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.image, self.rect = load_png(BALL_IMG)
         self.area = area
         if vector:
