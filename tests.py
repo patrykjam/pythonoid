16d558c6ea8f78133e02726d9354ca792ee51dc2
patrykvan <patrykvan@gmail.com>
2019-01-10 17:13:12 +0100

    Add test

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 
 import pygame
 
+from ball import Ball
 from block import Block
 from load_utils import load_png
 from player_screen import PlayerScreen
@@ -30,6 +31,23 @@ class TestPythonoid(unittest.TestCase):
             expected_balls *= expected_multiplication
             player_screen.multiply_balls()
             self.assertEqual(expected_balls, len(player_screen.balls))
+
+    def test_player_screen_mutliply_balls_random_initial(self):
+        pygame.display.set_mode((0, 0))
+        player_screen = PlayerScreen(pygame.Surface((0, 0)), (None, None))
+        tested_initial_number_of_balls_count = 6
+
+        for i in range(1, tested_initial_number_of_balls_count + 1):
+            start_balls = i
+            player_screen.balls = [Ball(None, None) for _ in range(start_balls)]
+            expected_balls = start_balls
+            expected_multiplication = 3
+            tested_multiplication_count = 6
+
+            for i in range(tested_multiplication_count):
+                expected_balls *= expected_multiplication
+                player_screen.multiply_balls()
+                self.assertEqual(expected_balls, len(player_screen.balls))
 
     def test_block_color_out_of_color_range(self):
         block = Block(0, 0, 0, 0)
